5807bf9ebe75afe7e15284d510c89e63d8815d6d
AngeloMelo <angelo.s.melo@gmail.com>
2017-03-17 17:40:46 -0300

    Adding photos endpoint and morgan

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var express = require('express');
-var app     = express();
-var port    = process.env.PORT || 8080;
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
+var port       = process.env.PORT || 8080;
+
+//handling root requests
+app.get('/', function(req, res){
+	res.send('API avaliable at http://localhost/'+ port +'/api');
+});
+
+//endpoint to photos
+app.get('/api/photos', function(req, res){
+	res.send('photos');
+});
+
 
 
 app.listen(port);
