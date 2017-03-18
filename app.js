6a4149f78c9a1901ecee2b44aeeb354ad41bf2ef
AngeloMelo <angelo.s.melo@gmail.com>
2017-03-17 23:49:21 -0300

    Configuring routes. Adding new endpoints

--- app.js
+++ app.js
@@ -4,17 +4,39 @@ var bodyParser = require('body-parser');
 var morgan     = require('morgan');
 var port       = process.env.PORT || 8080;
 
+
+//configuring body parser
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+
+//configuring logging
+app.use(morgan('dev'));
+
 //handling root requests
 app.get('/', function(req, res){
 	res.send('API avaliable at http://localhost/'+ port +'/api');
 });
 
-//endpoint to photos
-app.get('/api/photos', function(req, res){
+//configuring routes
+var apiRoutes = express.Router();
+
+//endpoint to authenticate a user
+apiRoutes.post('/authenticate', function(req, res) {
+	res.send('login');
+});
+
+//endpoint to create new user
+apiRoutes.post('/signup', function(req, res) {
+	res.send('signup');
+});
+
+//endpoint to show all photos
+apiRoutes.get('/photos', function(req, res){
 	res.send('photos');
 });
 
-
+//configuring all routes under /api/*
+app.use('/api', apiRoutes);
 
 app.listen(port);
 console.log('server is running...');
