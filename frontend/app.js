327d0a85f2548a93f75b4c9387e27a8e45c49c7a
AngeloMelo <angelo.s.melo@gmail.com>
2017-03-23 00:40:28 -0300

    adding routes for admin area and aircraft crud

--- frontend/app.js
+++ frontend/app.js
@@ -14,12 +14,28 @@ vs.config(function($routeProvider, $locationProvider){
 		controller:'UsersController',
 		templateUrl:'/views/home.html'
 	})
-	.when('/photos',{
-		controller:'UsersController',
-		templateUrl:'/views/photos.html'
+	.when('/photos', {
+		controller:'PhotoAdmController',
+		templateUrl: '/views/admin/photos.html'
 	})
 	.when('/upload',{
 		controller:'UsersController',
 		templateUrl:'/views/upload.html'
+	})
+	.when('/admin/home',{
+		controller:'AdminController',
+		templateUrl:'/views/admin/admin.html'
+	})
+	.when('/admin/aircraft',{
+		controller:'AircraftController',
+		templateUrl:'views/admin/aircraft.html'
+	})
+	.when('/admin/aircrafts/edit/:id',{
+		controller:'AircraftController',
+		templateUrl:'views/admin/aircraft_edit.html'
+	})
+	.when('/admin/aircrafts/add',{
+		controller:'AircraftController',
+		templateUrl:'views/admin/aircraft_add.html'
 	});
 });
